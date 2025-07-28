73e73760e65ddc991754e4c934561b14ae48c23b
AndreyMaraj <andrey_maraj@icloud.com>
2025-07-28 10:27:39 +0300

    #12. Оптимизация хранения фото

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import multer from 'multer'
 import path from 'path'
 import cors from 'cors'
-import fs from 'fs'
+import fs from 'fs/promises'
 
 const app = express(),
 	PORT = process.env.PORT ?? 3001,
@@ -18,72 +18,42 @@ app.use(cors())
 app.use(express.json())
 app.use('/media', express.static(uploadDir))
 
-fs.mkdirSync(uploadDir, { recursive: true })
-fs.mkdirSync(tempDir, { recursive: true })
+fs.mkdir(uploadDir, { recursive: true })
+fs.mkdir(tempDir, { recursive: true })
 
-app.post('/upload', upload.array('files'), (req, res) => {
-	const folderPath = req.body.folderPath ? decodeURI(req.body.folderPath) : '',
+app.post('/upload', upload.array('files'), async (req, res) => {
+	const folderPath = req.body.folderPath || '',
 		fullUploadDir = path.join(uploadDir, folderPath)
 
-	if (!req.files || (req.files as Express.Multer.File[]).length === 0) {
-		if (folderPath) {
-			fs.rmdir(fullUploadDir, { recursive: true }, err => {
-				if (err) {
-					res.status(500).send('Ошибка при удалении папки')
-				} else {
-					res.json([])
-				}
-			})
-		} else {
-			res.status(400).send('folderPath не может быть пустым, если не переданы файлы.')
-		}
-		return
-	}
+	try {
+		const files = req.files as Express.Multer.File[] || []
 
-	fs.mkdir(fullUploadDir, { recursive: true }, err => {
-		if (err) {
-			return res.status(500).send('Ошибка при создании папки')
-		}
-
-		fs.readdir(fullUploadDir, (err, files) => {
-			if (err) {
-				return res.status(500).send('Ошибка при чтении папки')
+		if (files.length === 0) {
+			if (folderPath) {
+				await fs.rm(fullUploadDir, { recursive: true, force: true })
+				res.json([])
+			} else {
+				res.status(400).send('folderPath не может быть пустым, если не переданы файлы.')
 			}
+		}
 
-			const deletePromises = files.map(file =>
-				new Promise((resolve, reject) =>
-					fs.unlink(path.join(fullUploadDir, file), err => {
-						if (err) {
-							reject(err)
-						} else {
-							resolve(true)
-						}
-					})
-				)
+		await fs.mkdir(fullUploadDir, { recursive: true })
+
+		await Promise.all((await fs.readdir(fullUploadDir)).map(file => fs.unlink(path.join(fullUploadDir, file))))
+
+		const results = await Promise.all(
+			files.map(file =>
+				fs.rename(path.join(tempDir, file.originalname), path.join(fullUploadDir, file.originalname))
+					.then(() => `${folderPath}/${file.originalname}`)
 			)
+		)
 
-			Promise.all(deletePromises)
-				.then(() => {
-					const filePromises = (req.files as Express.Multer.File[]).map((file: Express.Multer.File) => {
-						const filePath = path.join(fullUploadDir, file.originalname),
-							tempFilePath = path.join(tempDir, file.originalname)
-						return new Promise<string>((resolve, reject) =>
-							fs.rename(tempFilePath, filePath, err => {
-								if (err) {
-									reject(err)
-								} else {
-									resolve(`${folderPath}/${file.originalname}`)
-								}
-							})
-						)
-					})
-
-					return Promise.all(filePromises)
-				})
-				.then(results => res.json(results))
-				.catch(err => res.status(500).send('Ошибка при сохранении файлов'))
-		})
-	})
+		res.json(results)
+	}
+	catch (e) {
+		console.error('Ошибка при обработке файлов:', e)
+		res.status(500).send('Ошибка при обработке файлов')
+	}
 })
 
 app.listen(PORT, () => console.log(`Сервер запущен на http://localhost:${PORT}`))
